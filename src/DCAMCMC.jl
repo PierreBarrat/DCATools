443d5db9481da70b1b2b7bd126a706e685541b35
Pierre Barrat-Charlaix <p.barrat@live.fr>
2019-01-21 16:16:52 +0100

    DCAMCMC from branch parallel

--- src/DCAMCMC.jl
+++ src/DCAMCMC.jl
@@ -4,6 +4,9 @@ using DCATools
 using DelimitedFiles
 using Printf
 using Statistics
+using Distributed
+using Random
+using Profile
 
 export doMCMC, samplefromgraph!, estimatetau, autocorr
 
@@ -14,12 +17,32 @@ Takes file as input. Untested yet.
 """
 function doMCMC(graphfile::String,outfile::String, format::String, q::Int64,M::Int64,t::Int64 ; T= 10000, beta = 1.0, verbose = true, conf_init=rand(1:q, L))
     g = readparam(graphfile, format=format, q=q)
-    doMCMC(g, M, t, outfile=outfile, T=T, beta=beta, verbose=verbose, conf_init=conf_init)
+    smaple = doMCMC(g, M, t, outfile=outfile, T=T, beta=beta, verbose=verbose, conf_init=conf_init)
     return(sample)
 end
 
 """
-	doMCMC(graph::DCAgraph, M::Int64, tau::Int64 ; outfile="", T= 100000, beta = 1.0, verbose = false,conf_init=rand(1:graph.q, graph.L))
+"""
+function doMCMC_par(graph::DCAgraph, M::Int64, tau::Int64, nprocs::Int64; T= 50*tau, beta = 1.0, verbose = false)
+	if length(workers()) != nprocs
+		@warn "Number of workers ($(length(workers()))) different from `nprocs=$nprocs` value."
+	end
+	Mloc = cld(M, nprocs)
+	if Mloc != M/nprocs
+		@warn "Sample size `M=$M` not divisible by `nproc=$nprocs`"
+	end
+
+	if verbose
+		println("Starting $nprocs MCMC chains of $Mloc samples each.")
+	end
+	sample = @distributed (vcat) for n in 1:nprocs
+		doMCMC(graph, Mloc, tau, T = T, beta = beta, outfile="", verbose = false, conf_init = rand(1:graph.q, graph.L), nprocs = 1)
+	end
+	return sample
+end
+
+"""
+	doMCMC(graph::DCAgraph, M::Int64, tau::Int64 ; outfile="", T= 100000, beta = 1.0, verbose = false,conf_init=rand(1:graph.q, graph.L), nprocs = 1)
 
 Sample `M` configurations from probability distribution defined by `graph`. Number of MCMC steps between configurations is `tau`. 
 
@@ -28,14 +51,29 @@ Keyword parameters:
 - T: Default 50*tau. Initial number of iterations. 
 - beta: Default 1.0. Inverse temperature
 - conf_init: Default random. Initial configuration. 
+- nprocs: Default 1. Number of parallel MCMC chains.
 """
-function doMCMC(graph::DCAgraph, M::Int64, tau::Int64 ; outfile="", T= 50*tau, beta = 1.0, verbose = false,conf_init=rand(1:graph.q, graph.L))
+function doMCMC(graph::DCAgraph, M::Int64, tau::Int64 ; outfile="", T= 50*tau, beta = 1.0, verbose = false,conf_init=rand(1:graph.q, graph.L), nprocs = 1)
+
+	# Handling parallel case first
+	if nprocs > 1
+		if verbose
+			println("Parallel MCMC sampling with $nprocs workers.")
+		end
+		sample = doMCMC_par(graph, M, tau, nprocs, T = T, beta = beta, verbose=verbose)
+    	if outfile!=""
+    		writedlm(outfile,sample,' ')
+    	end
+		return sample
+	end
 
 	# Unnecessary allocation here?  
-	graph_local = deepcopy(graph)
     if beta != 1.
+		graph_local = deepcopy(graph)
         graph_local.J *= beta
         graph_local.h *= beta
+    else
+    	graph_local = graph
     end
 
     sample = zeros(Int64, M,graph_local.L)
@@ -57,7 +95,6 @@ function doMCMC(graph::DCAgraph, M::Int64, tau::Int64 ; outfile="", T= 50*tau, b
         samplefromgraph!(graph_local, sample[m,:], conf_end, tau)
         sample[m+1,:] .= conf_end
     end
-    verbose ? println("") : print("")
     verbose ? println("Done") : print("")
 
     # Writing output
@@ -79,24 +116,25 @@ function samplefromgraph!(g::DCAgraph, conf_init::Array{Int64,1}, conf_end::Arra
 
 	copyto!(conf_end, conf_init)
 	@fastmath @inbounds for t in 1:tau
-		i = rand(1:L)
-		a = conf_end[i]
-		b = rand(1:q)
-		while b==a
-			b = rand(1:q)
-		end
-        id_i_a = (i-1)*q+a
-        id_i_b = (i-1)*q+b
-        E = g.h[id_i_a] - g.h[id_i_b]
-        for j = 1:L
-            if j!=i
-                E += g.J[(j-1)*q+conf_end[j], id_i_a] - g.J[(j-1)*q+conf_end[j], id_i_b]
-            end
-        end
+		for i in 1:L
+			a = conf_end[i]
+			b = Random.rand(1:q) 
+			while b==a
+				b = Random.rand(1:q)
+			end
+        	id_i_a = (i-1)*q+a
+        	id_i_b = (i-1)*q+b
+        	E = g.h[id_i_a] - g.h[id_i_b]
+        	for j = 1:L
+        	    if j!=i
+        	        E += g.J[(j-1)*q+conf_end[j], id_i_a] - g.J[(j-1)*q+conf_end[j], id_i_b]
+        	    end
+        	end
 
-        if E<=0 || exp(-E) > rand(Float64) 
-        	conf_end[i] = b
-        end
+        	if E<=0 || exp(-E) > Random.rand() 
+        		conf_end[i] = b
+        	end
+    	end
 	end
 end
 
