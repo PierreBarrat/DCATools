2faabfebcf968b9f1aa69acf566620181cc7229f
Pierre Barrat <p.barrat@live.fr>
2021-12-01 14:47:05 +0100

    Remove reference to MersenneTwister
    default rng was updated in julia 1.7

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -108,7 +108,7 @@ end
 Sample for `tau` sweeps from probability defined by `g`, starting with configuration `conf_init` and storing final configuration in `conf_end`. 
 """
 function samplefromgraph!(g::DCAgraph, conf_init::Array{Int64,1}, conf_end::Array{Int64,1}, tau::Int64)
-	rng = MersenneTwister(rand(1:100000))
+	rng = rand(1:100000)
 	E = 0.
 	q = g.q
 	L = g.L
@@ -152,7 +152,7 @@ Sample for `round(tau*L)` swaps from probability defined by `g`, starting with c
 This version is used if `τ` is not an integer. It allows doing single swaps for `τ=1/L`  
 """
 function samplefromgraph!(g::DCAgraph, conf_init::Array{Int64,1}, conf_end::Array{Int64,1}, tau::Float64)
-	rng = MersenneTwister(rand(1:100000))
+	rng = rand(1:100000)
 	E = 0.
 	q = g.q
 	L = g.L
