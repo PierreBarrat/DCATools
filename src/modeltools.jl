88d44b15a5398b65d18982a0980685c639b57c13
PierreBarrat <p.barrat@live.fr>
2022-01-27 12:10:37 +0100

    improve docs / kwarg names for switchgauge!

--- src/modeltools.jl
+++ src/modeltools.jl
@@ -1,34 +1,37 @@
 export switchgauge!, computeenergies, inferprofile, pseudolikelihood
 
 """
-    switchgauge!(g::DCAgraph ; gauge="0sum")
+    switchgauge!(g::DCAgraph; gauge=:sum0, col=g.q, wt=Array{Int64,1}(undef,0))
 
-Switch parameters in `g` to gauge `gauge`. 
+Switch parameters in `g` to gauge `gauge`.
 Implemented gauges:
-1. 0 sum: "0sum"
-2. Lattice gas: "LG", "lg", "latticegas". 
+- 0 sum: `:sum0`
+- Lattice gas (state `col` has energy 0): `:LG`, `:lg`, `:latticegas`.
+- Wild type (sequence `wt` has energy 0): `:wt`
+-
+
 """
-function switchgauge!(g::DCAgraph; gauge="0sum", col=g.q, wt=Array{Int64,1}(undef,0))
-    if gauge=="0sum"
-        g.J,g.h = switchgauge0sum(g.J,g.h,g.L,g.q)
-    elseif gauge=="LG" || gauge=="lg" ||  gauge=="latticegas"
+function switchgauge!(g::DCAgraph; gauge=:sum0, col=g.q, wt=Array{Int64,1}(undef,0))
+    if gauge==:sum0
+        g.J,g.h = switchgaugesum0(g.J,g.h,g.L,g.q)
+    elseif gauge==:LG || gauge==:lg ||  gauge==:latticegas
         g.J,g.h = switchgaugeLG(g.J,g.h,g.q,col=col)
-    elseif gauge=="wt"
+    elseif gauge==:wt
         g.J, g.h = switchgaugeWT(g.J, g.h, wt, g.q)
-    elseif gauge=="ML" || gauge=="ml"
+    elseif gauge==:ML || gauge==:ml
         g.J, g.h = switchgaugeML(g.J, g.h, g.L, g.q)
     else
-        error("modeltools.jl - switchgauge: unrecognized `gauge` keyword.")
+        error("modeltools.jl - switchgauge: unrecognized `gauge=$(gauge)` keyword.")
     end
     return g
 end
 
 """
-    switchgauge0sum(J::Array{Float64,2}, h::Array{Float64,1}, L::Int64, q::Int64)
+    switchgaugesum0(J::Array{Float64,2}, h::Array{Float64,1}, L::Int64, q::Int64)
 
 Switch parameters to 0 sum gauge.
 """
-function switchgauge0sum(J::Array{Float64,2}, h::Array{Float64,1}, L::Int64, q::Int64)
+function switchgaugesum0(J::Array{Float64,2}, h::Array{Float64,1}, L::Int64, q::Int64)
     ho = zeros(Float64, L*q)
     Jo = zeros(Float64, L*q, L*q)
     t = zeros(Float64,q,q)
@@ -100,7 +103,7 @@ end
 """
 """
 function switchgaugeML(J, h, L, q)
-    Jo, ho = switchgauge0sum(J,h,L,q)
+    Jo, ho = switchgaugesum0(J,h,L,q)
     α = 1. /(q + L - 1)
     for i in 1:L
         for j in (i+1):L
