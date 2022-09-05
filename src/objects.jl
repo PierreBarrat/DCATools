1966533fb1c62934a97f24ac69e91cfe48c81eb6
PierreBarrat <p.barrat@live.fr>
2022-09-05 13:55:06 +0200

    add option for random initialization to DCAGraph

--- src/objects.jl
+++ src/objects.jl
@@ -1,29 +1,62 @@
+## This could be struct instead of mutable struct?
 """
 	mutable struct DCAGraph
 
 Structure representing a Potts model.
+Can be constructed with keywords, *e.g.* `DCAGraph(; L=2, q=2, J=..., h=...)`.
 
 ## Fields
 ```
-L::Int64
-q::Int64
+L::Int
+q::Int
 J::Array{Float64,2}
 h::Array{Float64,1}
 ```
 """
 Base.@kwdef mutable struct DCAGraph
-    L::Int64
-    q::Int64
+    L::Int
+    q::Int
     J::Array{Float64,2} = zeros(Float64, L*q, L*q)
     h::Array{Float64,1} = zeros(Float64, L*q)
 end
 """
-	DCAGraph(L,q)
+	DCAGraph(
+		L,q;
+		init = :null, Jrand = N -> 1/L*randn(N,N), hrand = N -> 1/sqrt(L)*randn(N),
+	)
 
-Creates a `DCAGraph` of the required size. Parameters are initialized to zero.
+Return a `DCAGraph` of the required size.
+- `init == :null`: parameters are intialized to zero.
+- `init == :rand`: parameters are randomly sampled using `Jrand` and `hrand` keywords.
+
+## Random initialization
+
+- `hrand` should be a function `N::Int -> h::Vector{Float64}` with `h` of size `N`.
+- `Jrand` should be a function `N::Int -> J::Matrix{Float64}` with `J` of size `NxN`.
+
+`Jrand` does not have to return a symetric matrix.
+The output matrix is made symetric with zeroes on the diagonal blocks.
+
 """
-function DCAGraph(L,q)
-	return DCAGraph(zeros(Float64, L*q, L*q), zeros(Float64, L*q), L, q)
+function DCAGraph(
+	L,q;
+	init = :null, Jrand = N -> 1/L*randn(N,N), hrand = N -> 1/sqrt(L)*randn(N),
+)
+	if init == :null
+		DCAGraph(L, q, zeros(Float64, L*q, L*q), zeros(Float64, L*q))
+	elseif init == :rand
+		return random_graph(L, q, Jrand, hrand)
+	end
+end
+
+function random_graph(L, q, Jrand, hrand)
+	h = hrand(L*q)
+	J = Jrand(L*q)
+	J .= (J .+ J')/2
+	for i in 1:L
+		J[(i-1)*q .+ (1:q), (i-1)*q .+ (1:q)] .= 0
+	end
+	return DCAGraph(; L, q, J, h)
 end
 
 
